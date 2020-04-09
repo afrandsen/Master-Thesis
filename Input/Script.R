61935e7ec7117c9ba5225e63e0f95261b9a1ba2a
Andreas Kracht Frandsen <buddif@gmail.com>
2020-04-09 21:52:51 +0200

    Theory

--- Input/Script.R
+++ Input/Script.R
@@ -1,8 +1,9 @@
 library(quantmod)
+library(moments)
 
 # LOAD DATA.
-input <- zoo::read.csv.zoo('Input/Samling.csv', format = "%Y%m%d")
-input <- as.xts(input)
+input <- zoo::read.csv.zoo('C:/Users/AKF/Documents/Matematik-Okonomi/5. Ar/2. Semester/Master-Thesis/Input/Samling.csv', format = "%Y%m%d")
+#input <- as.xts(input)
 
 # MANIPULATING DATA.
 
@@ -28,9 +29,12 @@ for (i in c(4:length(div))) {
 
 divPrice <- logDiv - log(as.numeric(input$vwindx[5:N]))
 
+EP <- log(input$earn) - log(as.numeric(input$vwindx))
 DP <- xts::xts(divPrice, index(input$vwretd[5:N]))
 BM <- input$bm
 AKT_VAR <- log(1 + input$svar)
+SMB <- input$smb
+HML <- input$hml
 
 # TERM STRUCTURE
 T_SPREAD <- input$t10y/100 - input$t90y/100
@@ -38,60 +42,62 @@ Y_SPREAD <- input$t5y/100 - input$t90y/100
 C_SPREAD <- input$baa/100 - input$t90y/100
 D_SPREAD <- input$baa/100 - input$aaa/100
 
-# MAKROØKONOMISKE
+# MAKROØKONOMISKE 
 FR <- input$fedfund/100
 
 # JUSTERING
-TB_J <- TB + (var(TB, na.rm = TRUE)/2)
+TB_J     <- TB + (var(TB, na.rm = TRUE)/2)
 NET_TB_J <- NET_TB + (var(NET_TB, na.rm = TRUE)/2)
-AKT_J <- AKT + (var(AKT, na.rm = TRUE)/2)
-S_OBL_J <- S_OBL + (var(S_OBL, na.rm = TRUE)/2)
-V_OBL_J <- V_OBL + (var(V_OBL, na.rm = TRUE)/2)
+AKT_J    <- AKT + (var(AKT, na.rm = TRUE)/2)
+S_OBL_J  <- S_OBL + (var(S_OBL, na.rm = TRUE)/2)
+V_OBL_J  <- V_OBL + (var(V_OBL, na.rm = TRUE)/2)
 
-data <- xts::xts(merge(NET_TB, AKT, S_OBL, V_OBL, DP, BM, AKT_VAR, T_SPREAD, Y_SPREAD, C_SPREAD, D_SPREAD, FR), index(NET_TB))
+# BESKRIVENDE STATISTIK
 
+## AKTIVKLASSER
+DATA <- data.frame(TB, NET_TB, AKT, S_OBL, V_OBL)
+DATA_J <- data.frame(TB_J, NET_TB_J, AKT_J, S_OBL_J, V_OBL_J)
+
+ADJ_DATA_MEAN <- colMeans(DATA_J)
+DATA_STD <- apply(DATA, 2, sd)
+DATA_QUANTILE <- apply(DATA, 2, quantile)
+DATA_SKEWNESS <- apply(DATA, 2, skewness)
+DATA_KURTOSIS <- apply(DATA, 2, kurtosis)
+
+DESCRIPTIVE <- data.frame(adj_mean=ADJ_DATA_MEAN,
+                          sd=DATA_STD,
+                          skew=DATA_SKEWNESS,
+                          kurt=DATA_KURTOSIS,
+                          quantile=t(DATA_QUANTILE))
+
+## TILSTANDSVARIABLE
+DATA_T <- xts::xts(merge(INF, EP, DP, BM, AKT_VAR, SMB, HML, FR, T_SPREAD, Y_SPREAD, C_SPREAD, D_SPREAD), index(INF))
+DATA_T <- data.frame(na.omit(DATA_T))
+
+DATA_T_MEAN <- colMeans(DATA_T)
+DATA_T_STD <- apply(DATA_T, 2, sd)
+DATA_T_QUANTILE <- apply(DATA_T, 2, quantile)
+DATA_T_SKEWNESS <- apply(DATA_T, 2, skewness)
+DATA_T_KURTOSIS <- apply(DATA_T, 2, kurtosis)
+
+DESCRIPTIVE_T <- data.frame(mean=DATA_T_MEAN,
+                          sd=DATA_T_STD,
+                          skew=DATA_T_SKEWNESS,
+                          kurt=DATA_T_KURTOSIS,
+                          quantile=t(DATA_T_QUANTILE))
+
+data <- xts::xts(merge(TB, NET_TB, AKT, S_OBL, V_OBL, EP, DP, BM, AKT_VAR, T_SPREAD, Y_SPREAD, C_SPREAD, D_SPREAD, FR, HML, SMB), index(NET_TB))
 data <- na.omit(data)
 
-# KITCHEN SINK
-fit_AKT <- lm(lag(AKT, 1) ~ DP + BM + AKT_VAR + T_SPREAD + Y_SPREAD + C_SPREAD + D_SPREAD + FR, data = data)
-fit_S_OBL <- lm(lag(S_OBL, 1) ~ DP + BM + AKT_VAR + T_SPREAD + Y_SPREAD + C_SPREAD + D_SPREAD + FR, data = data)
-fit_V_OBL <- lm(lag(V_OBL, 1) ~ DP + BM + AKT_VAR + T_SPREAD + Y_SPREAD + C_SPREAD + D_SPREAD + FR, data = data)
 
-m_fit <- lm(cbind(AKT, S_OBL, V_OBL) ~ lag(DP, 1) + lag(BM, 1) + lag(AKT_VAR, 1) + lag(T_SPREAD, 1) + lag(Y_SPREAD, 1) + lag(C_SPREAD, 1) + lag(D_SPREAD, 1) + lag(FR, 1), data = data)
-
-
-library(vars)
-
-var <- VAR(data, p = 1)
-
-
-
-
-# LOAD DATA
-
-rf <- read.csv('Input/Riskfree.csv')
-stock <- read.csv('Input/Stock.csv')
-yield <- read.csv('Input/Yield.csv')
-
-rf
-
-(mean(log(1+rf$t90ret), na.rm = TRUE)+(var(log(1+rf$t90ret), na.rm=TRUE)/2))*12*100
-
-# DATA MANIPULATION
-
-## RISKFREE
-date <- zoo::as.Date(as.character(rf$caldt), "%Y%m%d")
-tbill90 <- as.numeric(rf$t90ret)
-
-rf <- xts::xts(order.by = date, tbill90)
-
-log_rf <- log(1+rf)
-
-## STOCK
-stockret <- as.numeric(stock$vwretd)
-
-stock <- xts::xts(order.by = date, stockret)
-
-log_stock <- log(1+stock)
-
-plot(log(1+stock$vwretd), type = 'l')
+# # KITCHEN SINK
+# fit_AKT <- lm(lag(AKT, -1) ~ DP + BM + AKT_VAR + T_SPREAD + Y_SPREAD + C_SPREAD + D_SPREAD + FR, data = data)
+# fit_S_OBL <- lm(lag(S_OBL, -1) ~ DP + BM + AKT_VAR + T_SPREAD + Y_SPREAD + C_SPREAD + D_SPREAD + FR, data = data)
+# fit_V_OBL <- lm(lag(V_OBL, -1) ~ DP + BM + AKT_VAR + T_SPREAD + Y_SPREAD + C_SPREAD + D_SPREAD + FR, data = data)
+# 
+# m_fit <- lm(cbind(AKT, S_OBL, V_OBL) ~ lag(DP, 1) + lag(BM, 1) + lag(AKT_VAR, 1) + lag(T_SPREAD, 1) + lag(Y_SPREAD, 1) + lag(C_SPREAD, 1) + lag(D_SPREAD, 1) + lag(FR, 1), data = data)
+# 
+# 
+# library(vars)
+# 
+# var <- VAR(data, p = 1)
